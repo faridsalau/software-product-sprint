73929e71cb85f15e539176a18001b348de7ba966
Farid Salau <43255135+faridsalau@users.noreply.github.com>
2020-03-12 15:59:19 -0500

    Update FindMeetingQuery.java

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -43,6 +43,7 @@ public final class FindMeetingQuery {
           for(String attendee: attendees) {
               if(requestAttendees.contains(attendee)) {
                 eventList.add(event.getWhen());
+                break;
               }
           }
       }
